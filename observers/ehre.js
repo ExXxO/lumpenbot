1811ecb1571f4caa3113dae684d68afb1104ca45
Ian <jan.sidler@bluewin.ch>
2020-02-18 18:18:09 +0100

    Update ehre.js

--- observers/ehre.js
+++ observers/ehre.js
@@ -1,25 +1,18 @@
-/*
- * Observer class
- * -----------------------------------------------------------------------------
- * Notices when people are talking about a certain keyword
- *
- */
+module.exports = function (client, from, to, text, message) {
 
-module.exports = function(client, from, to, text, message) {
+    var lowerCasedText = text.toLowerCase();
+    var observerWords = [
+        'ehre',
+    ]
 
-  var lowerCasedText = text.toLowerCase();
-  var observerWords = [
-    'ehre',
-  ]
+    if (new RegExp(observerWords.join("|")).test(lowerCasedText)) {
 
-  if (new RegExp(observerWords.join("|")).test(lowerCasedText)) {
+        var observerTexts = [
+            'lumpenLove',
+        ];
 
-    var observerTexts = [
-      'lumpenLove',
-    ];
-
-    // If u wanna mention the user
-    //return from+': '+observerTexts[Math.floor(Math.random()*observerTexts.length)];
-    return observerTexts[Math.floor(Math.random()*observerTexts.length)];
-  }
+        // If u wanna mention the user
+        //return from+': '+observerTexts[Math.floor(Math.random()*observerTexts.length)];
+        return observerTexts[Math.floor(Math.random() * observerTexts.length)];
+    }
 };
